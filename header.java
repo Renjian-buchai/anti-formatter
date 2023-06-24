88237ed2cd693d51b623a1227efd9162bc2d8639
Renjian-buchai <tyiwei0@gmail.com>
2023-06-24 11:32:53 +0800

    don't touch, especially the ternary thing

--- header.java
+++ header.java
@@ -19,6 +19,8 @@ void is_charLiteral(const char&, uint8_t&);
 void is_escapeSequence(const char&, uint8_t&);
 void preprocessor(
     const char&, uint8_t&, std::string&, uint8_t&, std::vector<token>&, token new_token = token());
-void file_creator(std::vector<token>&, uint8_t&, uint32_t&, std::ofstream&, uint32_t&, uint32_t&);
+void file_creator(
+    std::vector<token>&, uint8_t&, const uint32_t&, std::ofstream&, uint32_t&, const uint32_t&);
+void flush_andNew(std::string&, uint8_t&, std::vector<token>&, char&, const uint8_t);
 
 #endif
